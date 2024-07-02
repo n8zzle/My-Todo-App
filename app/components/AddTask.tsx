61b34c46cc39a6c83ec302df0cec9cb3a0c9bfde
n8zzle <playwithn8zzle@gmail.com>
2024-07-02 16:09:31 +0200

    Adding React Icons library

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa";
 
 type Props = {};
 
 const AddTask = (props: Props) => {
   return (
     <div>
-      <button className="btn btn-primary w-full">Add new task</button>
+      <button className="btn btn-primary w-full">
+        Add new task <FaPlus />
+      </button>
     </div>
   );
 };
