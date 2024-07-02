c53fd4061f83f965b43254f2dd8529fb2dfbb4ac
n8zzle <playwithn8zzle@gmail.com>
2024-07-02 16:04:03 +0200

    new projext

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const AddTask = (props: Props) => {
+  return (
+    <div>
+      <button className="btn btn-primary w-full">Add new task</button>
+    </div>
+  );
+};
+
+export default AddTask;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import AddTask from "./components/AddTask";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Todo List App</h1>
+        <AddTask />
+      </div>
+    </main>
+  );
+}
