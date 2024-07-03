d9636c5f1c5e0ece8fb57eaf4325c95ec3d0ae87
n8zzle <playwithn8zzle@gmail.com>
2024-07-03 15:42:20 +0200

    Adding json-server , and fetching data from data/todo.json

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,38 +1,27 @@
+import { ITask } from "@/types/tasks";
 import React from "react";
 
-type Props = {};
-
-const TodoList = (props: Props) => {
+interface TodoListProps {
+  tasks: ITask[];
+}
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table">
         {/* head */}
         <thead>
           <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
+            <th>Task</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <th>3</th>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-          </tr>
+          {tasks.map((task) => (
+            <tr key={task.key}>
+              <td>{task.text}</td>
+              <td>Completed</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
@@ -9,7 +12,7 @@ export default function Home() {
         <AddTask />
       </div>
       <div>
-        <TodoList />
+        <TodoList tasks={tasks} />
       </div>
     </main>
   );
